f2970e579fff778c07bfc5b23e8d6203f703b531
george <gkephart@cnm.edu>
2019-03-14 21:32:59 -0600

    finalized the map example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MapService} from "ngx-mapbox-gl";
+import {faPaw} from "@fortawesome/free-solid-svg-icons";
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: "./app.component.html",
-  styles: [ `    
-    mgl-map {
-   height: 80vh;
-   width: 80vw;
-  }
-  `]
+	selector: 'app-root',
+	templateUrl: "./app.component.html",
+	styles: [`
+		mgl-map {
+			height: 80vh;
+			width: 80vw;
+		}
+
+		mgl-marker {
+			width: 15px;
+			height: 15px;
+			border-radius: 50%;
+			background-color: red;
+		}
+	`]
 
 })
 export class AppComponent {
-  map : any;
+
+	Geo
+	map: any;
+	lat: number = 35.0856181;
+	lng: number = -106.6493357;
+	fawPaw = faPaw;
+
+
+
+	points: any = [
+		{name: "one", lng: -106.6495457, lat: 35.0356181},
+		{name: "two", lng: -106.6503457, lat: 35.0556161},
+		{name: "three", lng: -106.6513557, lat: 35.0756131},
+		{name: "two", lng: -106.6493757, lat: 35.0956141},
+	];
+
+	constructor(private mapService: MapService) {
+	}
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {NgxMapboxGLModule} from "ngx-mapbox-gl";
+import {MapService, NgxMapboxGLModule} from "ngx-mapbox-gl";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 let ngxMapboxConfig = NgxMapboxGLModule.withConfig({
 	accessToken: 'pk.eyJ1IjoiZ2Vvcmdla2VwaGFydCIsImEiOiJjanQ4cmdmYjkwYnZnNDNwNDF4NXFiMTJmIn0.MwDDiyszR0QFmMYMNvzi1Q',
@@ -10,9 +11,9 @@ let ngxMapboxConfig = NgxMapboxGLModule.withConfig({
 });
 
 @NgModule({
-	declarations: [AppComponent ],
-	imports: [BrowserModule, ngxMapboxConfig],
-	providers: [],
+	declarations: [AppComponent,   ],
+	imports: [BrowserModule, ngxMapboxConfig, FontAwesomeModule],
+	providers: [MapService],
 	bootstrap: [AppComponent]
 })
 export class AppModule {
